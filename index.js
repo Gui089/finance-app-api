2a83266a78aaa75a054168987078ad3d482d5dae
Gui089 <guilhermesilba811@gmail.com>
2024-05-20 20:32:02 -0300

    Setup initial

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+import express from 'express';
+import 'dotenv/config.js';
 
+import{ PostgresHelper} from './src/db/postgres/helper.js';
 
+const app = express();
+
+app.get('/', async (req, res) => {
+
+    const results = await PostgresHelper.query('SELECT * FROM users;');
+
+    res.send(JSON.stringify(results));
+});
+
+app.listen(3000, () => console.log('listening on port 3000'));
