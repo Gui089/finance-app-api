91f2eb258a97daa32cff9df831c0602a9343dc23
Gui089 <guilhermesilba811@gmail.com>
2024-06-03 21:22:37 -0300

    :recycle: Refact: in create-user controller and created helper.js with functions of status code

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,63 +1,38 @@
 import { CreateUserCase } from "../use-cases/create-user.js";
 import  validator  from 'validator';
+import { badRequest, created, serverError } from "./helper.js";
 
 export class CreateUserController {
     async execute(httpRequest) {
         try {
             const params = httpRequest.body;
     
-            //validar a requisisção (Campos obrigatórios e tamanho de senha)
-    
             const requiredFields = ['first_name', 'last_name', 'email', 'password'];
             
             for(const field of requiredFields) {
                 if(!params[field] || params[field].trim().length === 0) {
-                    return {
-                        statusCode:400,
-                        body: {
-                            message: `Missing param: ${field}`
-                        },
-                    }
+                    return badRequest({message:`Missing param ${field}`});
                 }
             }
 
             if(params.password.length < 6) {
-                return {
-                    statusCode:400,
-                    body: {
-                        errorMessage: 'Password must be at least 6 characters'
-                    }
-                }
+                return badRequest({message: 'Password must be at least 6 characters'});
             }
 
             const emailValid = validator.isEmail(params.email);
 
             if(!emailValid) {
-                return {
-                    statusCode:400,
-                    body: {
-                        errorMessage: 'Invalid email'
-                    }
-                }
+                return badRequest({message:'Invalid email'});
             }
     
-            //chamar o use-case
     
             const createUserUseCase = new CreateUserCase(); 
     
             const createdUser = await createUserUseCase.execute(params);
     
-            return {
-                statusCode: 201,
-                body: createdUser
-            }
+            return created(createdUser);
         } catch(error) {
-                return {
-                    statusCode:500,
-                    body: {
-                        message: `internal server error ${error.message}`
-                    }
-                }
+                return serverError();
         }
     }
 }
